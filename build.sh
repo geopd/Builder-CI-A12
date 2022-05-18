d450f6171f83a781d6d1b1f6d129012625256896
GeoPD <geoemmanuelpd2001@gmail.com>
2022-05-18 20:09:59 +0530

    builder-ci: Initialize a auto build post generator
    - Guarded for personal use; since post format differs for people.
    
    - Post format: https://t.me/mystohub/34042
    
    - Includes: Post image, needed details, download link, checksum,
                changelogs generated from source repos(telegraph api),
                build time and HEAD sha of device,vendor and kernel repos.
                (for now)
    
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- build.sh
+++ build.sh
@@ -71,7 +71,18 @@ tree_path() {
 	KERNEL_TREE=kernel/xiaomi/msm8953
 }
 
-# Clone needed misc scripts and ssh priv keys
+# Build post-gen variables (optional)
+lazy_build_post_var() {
+	LAZY_BUILD_POST=true
+	ANDROID_VERSION="Android 12L"
+	RELEASE_TYPE="Test"
+	DEV=GeoPD
+	TG_LINK=https://t.me/mysto_o
+	GRP_LIN="@mystohub"
+	DEVICE2=daisa #Since I do unified builds for daisy&sakura
+}
+
+# Clone needed misc scripts and ssh priv keys (optional)
 clone_file() {
 	rclone copy brrbrr:scripts/setup_script.sh /tmp/rom
 	rclone copy brrbrr:ssh/ssh_ci /tmp/rom
@@ -157,6 +168,14 @@ telegram_build() {
 	-F caption="$2"
 }
 
+telegram_build_post() {
+	curl -s -F "photo=@$1" "https://api.telegram.org/bot${BOTTOKEN}/sendPhoto" \
+	-F chat_id="${CHATID}" \
+	-F "disable_web_page_preview=true" \
+	-F "parse_mode=Markdown" \
+	-F caption="$2"
+}
+
 # Send Telegram posts for sync finished, build finished and error logs
 telegram_post_sync() {
 	telegram_message "
@@ -194,12 +213,90 @@ compiled_zip() {
 	echo "${ZIP}"
 }
 
+# Generate changelog of past 7 days
+generate_changelog() {
+	CHANGELOG=$(pwd)/changelog_gen.txt
+	touch ${CHANGELOG}
+	echo "Generated Date: $(date)" >> ${CHANGELOG}
+
+	for i in $(seq 7);
+	do
+		after_date=`date --date="$i days ago" +%F`
+		until_date=`date --date="$(expr ${i} - 1) days ago" +%F`
+		echo "====================" >> ${CHANGELOG}
+		echo "     $until_date    " >> ${CHANGELOG}
+		echo "====================" >> ${CHANGELOG}
+		while read path; do
+			git_log=`git --git-dir ./${path}/.git log --after=$after_date --until=$until_date --format=tformat:"%s [%an]"`
+			if [[ ! -z "${git_log}" ]]; then
+			echo "* ${path}" >> ${CHANGELOG}
+			echo "${git_log}" >> ${CHANGELOG}
+			echo "" >> ${CHANGELOG}
+			fi
+		done < ./.repo/project.list
+	done
+}
+
+# Generate telegraph post of changelog
+telegraph_post() {
+	generate_changelog
+	sed -i "s/\"/'/g" ${CHANGELOG}
+	curl -X POST \
+		-H 'Content-Type: application/json' \
+		-d '{
+			"access_token": "'${TGP_TOKEN}'",
+			"title": "Source Changelogs",
+			"author_name":"geopd",
+			"content": [{"tag":"p","children":["'"$(cat ${CHANGELOG})"'"]}],
+			"return_content":"true"
+		}' \
+		https://api.telegra.ph/createPage | cut -d'"' -f12
+}
+
+# Aah yes! Generate a build post that's similiar to normal official posts
+# Now no more pain of making posts; just forward and Enjoy.
+lazy_build_post() {
+	rclone copy brrbrr:images/ $(pwd)
+	if [ -f $(pwd)/${NAME}* ]; then
+		POST_IMAGE=$(pwd)/${NAME}*
+	else
+		POST_IMAGE=$(pwd)/mystohub*
+	fi
+	T_NAME=$(echo ${NAME,,}| cut -d'-' -f1)
+
+	telegram_build_post ${POST_IMAGE} "
+	*#${T_NAME} #ROM #$(echo ${ANDROID_VERSION,,} | tr -d ' ') #${T_DEVICE,,} #${DEVICE2,,}
+	$(echo ${ZIPNAME^^}| cut -d'-' -f1) | ${ANDROID_VERSION^}
+	Updated:* \`$(date +"%d-%B-%Y")\`
+	*By:* [${DEV}](${TG_LINK})
+
+	*▪️ Downloads:* [Vanilla](${DWD}) *(${ZIPSIZE})*
+	*▪️ Changelogs:* [Source Changelogs]($(echo $(telegraph_post) | tr -d '\\'))
+	*▪️ ROM:* \`${ZIPNAME}\`
+	*▪️ MD5 Checksum:* \`${MD5CHECK}\`
+
+	*Notes:*
+	• ${ANDROID_VERSION} ${RELEASE_TYPE} Release.
+
+	*Build Info:*
+	• *✅ Build finished after $(($BDIFF / 3600)) hrs : $(($BDIFF % 3600 / 60)) mins : $(($BDIFF % 60)) secs*
+	• *Commit SHA:* \`$(commit_sha)\`
+
+	*Follow* ${GRP_LIN}
+	*Join* ${GRP_LIN}
+	*Date:*  \`$(date +"%d-%m-%Y %T")\`" &> /dev/null
+}
+
 # Post Build finished with Time,duration,md5,size&Tdrive link OR post build_error&trimmed build.log in TG
 telegram_post(){
 	if [ -f $(pwd)/out/target/product/${T_DEVICE}/${ZIPNAME} ]; then
 		rclone copy ${ZIP} brrbrr:rom -P
 		DWD=${TDRIVE}${ZIPNAME}
-		telegram_post_build
+		if [[ $GIT_USER = GeoPD && $LAZY_BUILD_POST = true ]]; then
+			lazy_build_post
+		else
+			telegram_post_build
+		fi
 	else
 		echo "CHECK BUILD LOG" >> $(pwd)/out/build_error
 		ERROR_LOG=$(pwd)/out/build_error
@@ -214,6 +311,7 @@ compile_moments() {
 	git_setup
 	if [ $GIT_USER = GeoPD ]; then
 		clone_file
+		lazy_build_post_var
 	fi
 	ssh_authenticate
 	time_sec SYNC_START
